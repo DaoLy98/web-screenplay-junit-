12dfe2c29f159ce54033dddeb0f6030479145896
DaoLy98 <daothily1602@gmail.com>
2021-07-17 09:29:21 +0700

    bai 11 tìm kiếm web element sử dụng Xpath Axes

--- src/test/java/vn/fado/pages/HomPage.java
+++ src/test/java/vn/fado/pages/HomPage.java
@@ -1,9 +1,16 @@
 package vn.fado.pages;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 
 
 public class HomPage extends PageObject{
-
+	@FindBy(xpath = "//a[contains(text(),'Đăng nhập')]")
+	WebElementFacade loginLink;
+	public void clickOnLoginLink() {
+		loginLink.click();
+	}
+	
 }
